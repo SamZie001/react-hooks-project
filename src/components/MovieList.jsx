87580f7b44e866175b7e5888b2154f4b5cdeae4f
SamZie001 <samzie001@gmail.com>
2022-05-07 06:46:56 +0100

    updated

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react'
-import MovieCard from './MovieCard'
+import React from 'react'
 
 function MovieList({list}) {
 
-
   return (
     <div>
-        {
-            list.map((movie)=>(
-                <div key={movie.id} className='mapped'>
-                    <img className='image' src={movie.posterURL} alt='img' />
-                    <h5 className='mov'>{movie.id}. {movie.title}</h5>
-                    <h4><h5> Desc - {movie.description}</h5></h4>
-                    <h4><h5>Rating - {movie.rating}</h5></h4>
-                    
-                </div>
-            ))
-        }
+      {
+        list.map((movie)=>(
+            <div key={movie.id} className='mapped'>
+              
+              <img className='image' src={movie.posterURL} alt='img' />
+              <h5 className='mov'><span>{movie.id}</span>.{movie.title}</h5>
+              
+              <h5 className='dd' > <span>Description</span>: {movie.description}</h5>
+              <h5 className='dd'> <span>Rating</span>: {movie.rating}</h5>
+
+            </div>
+        ))
+      }
     </div>
   )
 }
